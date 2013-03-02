e8ba09f30b89a162d654bb72ae860a83cd83c6f1
Smit Patel <smit1293@gmail.com>
2013-03-02 17:04:37 -0500

    Temporary remove :recoverable module

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Smitinterest::Application.configure do
+ Smitinterest::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
